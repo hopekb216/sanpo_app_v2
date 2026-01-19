3e9cff9a84c4ce2bcaa8256e77bfc7d55f7e894a
hiro <hope_3722hk@yahoo.co.jp>
2026-01-19 19:59:20 +0900

    Fix redirect after sign up and sign in

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,22 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  protected
+
+  # サインアップ後（メール認証なし）
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
+  # サインアップ後（メール認証ありの場合はこちらが呼ばれる）
+  def after_inactive_sign_up_path_for(resource)
+    root_path
+  end
+
+  # ログイン後の遷移先
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  # プロフィール更新処理（あなたの元コードをそのまま保持）
   def update
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
 
@@ -11,4 +29,3 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 end
-
