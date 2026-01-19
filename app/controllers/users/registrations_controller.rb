86e4cda5cd71b3961fb2da930c28e27caf25c94a
hiro <hope_3722hk@yahoo.co.jp>
2026-01-19 20:09:29 +0900

    Deviseの新規登録後リダイレクトをTurbo対応で修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,7 +16,40 @@ class Users::RegistrationsController < Devise::RegistrationsController
     root_path
   end
 
-  # プロフィール更新処理（あなたの元コードをそのまま保持）
+  public
+
+  # Turbo 対応の create
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+
+    if resource.persisted?
+      if resource.active_for_authentication?
+        sign_up(resource_name, resource)
+        respond_to do |format|
+          format.turbo_stream { redirect_to root_path }
+          format.html { redirect_to root_path }
+        end
+      else
+        expire_data_after_sign_in!
+        respond_to do |format|
+          format.turbo_stream { redirect_to root_path }
+          format.html { redirect_to root_path }
+        end
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_to do |format|
+        format.turbo_stream { render :new, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # プロフィール更新処理
   def update
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
 
